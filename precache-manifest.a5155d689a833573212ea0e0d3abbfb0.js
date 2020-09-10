f7d04a0f50ff11083c446eebb0741e3be3596aba
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-09-10 17:28:15 +0300

    Updates

---
+++ precache-manifest.a5155d689a833573212ea0e0d3abbfb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c11e9a5e2bf97fc96ae6be7e66eb0b",
+    "url": "/CRUD-React-App/index.html"
+  },
+  {
+    "revision": "8bccbc7c84cc02f6451e",
+    "url": "/CRUD-React-App/static/css/main.a6e5c98b.chunk.css"
+  },
+  {
+    "revision": "cdca5ac464f617e86fb6",
+    "url": "/CRUD-React-App/static/js/2.9f61d41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-React-App/static/js/2.9f61d41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bccbc7c84cc02f6451e",
+    "url": "/CRUD-React-App/static/js/main.2c1de99a.chunk.js"
+  },
+  {
+    "revision": "a57b3b73b6911d6a8bef",
+    "url": "/CRUD-React-App/static/js/runtime-main.db2d85d1.js"
+  }
+]);
